fe7044909118940f1f81c2ba3ef5e470489daa5e
EManicaro <enrico.manicaro.a103782@mcast.edu.mt>
2017-04-11 15:44:49 +0200

    objects with score
    hitting a gd object = +1, hitting bad obejct = gameover, take all the
    good object = you win scene with a play again button

---
+++ Assets/Scripts/ColliderController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ColliderController : MonoBehaviour
+{
+
+    // this is the player score
+    public int score = 0;
+
+    // this is the canvas text box
+    public Text scoreText;
+
+    // this variable gives us the number of good objects in the scene
+    protected int numOfGoodObj;
+
+    // when the script launches, get the number of spheres
+    void Start()
+    {
+        // this gives us the number of good objects on the scene.
+        numOfGoodObj = GameObject.FindGameObjectsWithTag("GoodObj").Length;
+    }
+
+    // when the player hits a good obejct with a trigger collider, do something
+    void OnTriggerEnter2D (Collider2D c)
+    {
+
+        // check that the player has hit a good object
+        if (c.tag == "GoodObj")
+        {
+
+            // increment the score when the player hits the good object
+            score += 1;
+
+            // adjust the score on the canvas
+            scoreText.text = "Score: " + score;
+
+            // disabled the collider so it doesn't repeat
+            c.enabled = false;
+
+            // destroy the good object so we can't hit it a second time
+            Destroy(c.gameObject);
+
+            // show us the score in the console (Ctrl + Shift + C in Unity
+            Debug.Log(score);
+
+            //check that the score is the same as the number of good objects, and end the game.
+            if (score == numOfGoodObj)
+            {
+                SceneManager.LoadScene("YouWin Scene");
+            }
+
+        }
+        else
+        {
+
+            // if the player hits anything that is not the sphere, show the game over screen
+            SceneManager.LoadScene("GameOver Scene");
+
+            // in order to load this scene, we need to set it up in the Build Settings
+
+        }
+
+    }
+
+
+}
