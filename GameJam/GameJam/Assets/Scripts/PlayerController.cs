965739361a6212575d2dc6714351e9ad58673cbe
ligrima <lianagrima@gmail.com>
2017-04-12 10:51:28 +0200

    you win, game over, menu
    all are consistent - same colours, same fonts, same sizes, buttons all
    working :dancer:

--- GameJam/GameJam/Assets/Scripts/PlayerController.cs
+++ GameJam/GameJam/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,9 @@ public class PlayerController : MonoBehaviour {
     // The character's jump force
     float jumpForce = 300f;
 
+    // This is the character's animator
+    Animator anim;
+
     // This is the character's physics component
     Rigidbody2D playerRB;
 
@@ -20,6 +23,9 @@ public class PlayerController : MonoBehaviour {
 
     // This is the canvas text box
     public Text lifeText;
+
+    // This is the character's sprite rendered
+    SpriteRenderer spriteRenderer;
 
     // The ground checking object
     Transform groundCheck;
@@ -33,7 +39,9 @@ public class PlayerController : MonoBehaviour {
     // When the script is activated, get its components
     void Awake()
     {
+        anim = GetComponent<Animator>();
         playerRB = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
         groundCheck = transform.Find("groundCheck");
         defaultPos = transform.position;
@@ -43,7 +51,8 @@ public class PlayerController : MonoBehaviour {
     void FixedUpdate()
     {
         isGrounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
-        
+        anim.SetBool("isGrounded", isGrounded);
+
         //this tackles the left and right arrow keys
         float h = Input.GetAxisRaw("Horizontal");
 
@@ -51,7 +60,7 @@ public class PlayerController : MonoBehaviour {
         Move(h);
 
         // make the character jump!
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             playerRB.AddForce(Vector2.up * jumpForce);
         }
@@ -97,7 +106,17 @@ public class PlayerController : MonoBehaviour {
         // Make the character move
         playerRB.velocity = movement;
 
-        
+        //check that the player is looking in the direction he's moving
+        if ((h < 0f && !spriteRenderer.flipX) || (h > 0f && spriteRenderer.flipX))
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
 
+        //if h is zero, character is still
+        // if h is not zero, character is moving
+        bool isWalking = (h != 0f);
+
+        //set the boolean value to the condition above
+        anim.SetBool("isWalking", isWalking);
     }
-}
+  }
